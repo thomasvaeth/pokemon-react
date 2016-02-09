dde5c6b06d29683b5ba9916262e03bf87560433c
thomasvaeth <thomas.vaeth@gmail.com>
2016-02-08 17:41:12 -0800

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Gotta Catch Em All.</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
