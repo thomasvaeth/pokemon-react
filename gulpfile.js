8eb9cdad2cedb1aa72b0ec78d9a3c4b7cb8649ff
thomasvaeth <thomas.vaeth@gmail.com>
2016-02-10 16:52:01 -0800

    Changed Gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
 	return gulp.src('./css/main.scss'
 	).pipe(sass().on('error', sass.logError)
-	).pipe(gulp.dest('./css'));
+	).pipe(minifyCss({compatibility: 'ie8'})
+	).pipe(gulp.dest('./css')
+	);
 });
 
 gulp.task('build', ['sass']);
