34ddf427c5096c2adc8d1d2e9e6edc8e90ecd217
thomasvaeth <thomas.vaeth@gmail.com>
2016-02-10 20:19:09 -0800

    I broke something.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
 	return gulp.src('./css/main.scss'
 	).pipe(sass().on('error', sass.logError)
 	).pipe(minifyCss({compatibility: 'ie8'})
+	).pipe(rename('main.min.css')
 	).pipe(gulp.dest('./css')
 	);
 });
